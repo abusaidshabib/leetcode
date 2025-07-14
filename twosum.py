8d9e9e546c75ed3250590806bacaf41f72171ef9
abusaidshabib <abusaidshabib712@gmail.com>
2025-07-14 16:22:01 +0600

    twosum done

---
+++ twosum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in nums:
+            for j in nums:
+                if i + j == target:
+                    return [nums.index(i), nums.index(j)]
+
+
+
+nums = [2, 7, 11, 15]
+target = 9
+sol = Solution()
+print(sol.twoSum(nums, target))
