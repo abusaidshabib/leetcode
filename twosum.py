5ca4dde37a9725ad1659036f3e534cb6f6a0b3d8
abusaidshabib <abusaidshabib712@gmail.com>
2025-07-14 16:43:37 +0600

    two some second solution added

--- twosum.py
+++ twosum.py
@@ -1,14 +1,26 @@
 # shabib solution 1
+# class Solution(object):
+#     def twosum(self, nums, target):
+#         for i in nums:
+#             for j in nums:
+#                 if i + j == target:
+#                     return [nums.index(i), nums.index(j)]
+
+# shabib solution 2
 class Solution(object):
-    def twoSum(self, nums, target):
-        for i in nums:
-            for j in nums:
-                if i + j == target:
-                    return [nums.index(i), nums.index(j)]
+    def twosum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
 
 
 
-nums = [2, 7, 11, 15]
-target = 9
+
+# nums = [2, 7, 11, 15]
+nums = [4, 3, 90, -3]
+target = 0
 sol = Solution()
-print(sol.twoSum(nums, target))
+print(sol.twosum(nums, target))
