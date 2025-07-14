27157ce05af2cd3e2467323de9034561a7156a6a
abusaidshabib <abusaidshabib712@gmail.com>
2025-07-14 20:04:10 +0600

    is palindrome done

---
+++ romantoint.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/roman-to-integer/description/
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {
+            "I": 1,
+            "V":5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        first_v = None
+        result = 0
+        for i in reversed(s):
+            if not first_v:
+                first_v = i
+                result = roman[i]
+            elif first_v and roman[first_v] < roman[i]:
+                result += roman[i]
+            elif first_v and roman[first_v] > roman[i]:
+                result -= roman[i]
+            elif roman[first_v] == roman[i]:
+                result += roman[i]
+            print(result)
+
+            first_v = i
+
+        return result
+
+
+
+
+target = "DCCCXC"
+sol = Solution()
+print(sol.romanToInt(target))
